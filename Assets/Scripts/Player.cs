eac982d8919e1aa16155077b467cc5b312c70a60
Mojopon <tyamuro@gmail.com>
2015-11-20 09:58:39 +0900

    first commit
    first commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(PlayerController))]
+public class Player : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float turnSpeed = 180f;
+    PlayerController controller;
+
+    void Start()
+    {
+        controller = GetComponent<PlayerController>();
+    }
+
+    void Update()
+    {
+        var horizontalInput = Input.GetAxisRaw("Horizontal");
+        controller.Rotate(turnSpeed * horizontalInput);
+        var velocity = transform.rotation * Vector3.up * moveSpeed;
+        controller.Move(velocity);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerController : MonoBehaviour {
+
+    Vector2 velocity;
+    float swing;
+
+    Rigidbody2D myRigidBody;
+
+	void Start () {
+        myRigidBody = GetComponent<Rigidbody2D>();
+	}
+	
+	public void Move(Vector2 _velocity)
+    {
+        velocity = _velocity;
+    }
+
+    public void Rotate(float _swing)
+    {
+        swing = -_swing;
+    }
+
+    void Update()
+    {
+        //transform.Rotate(new Vector3(0, 0, 1), -swing * Time.deltaTime);
+    }
+
+    void FixedUpdate()
+    {
+        Quaternion rotation = transform.rotation;
+        float z = rotation.eulerAngles.z;
+        z += swing * Time.fixedDeltaTime;
+        myRigidBody.MoveRotation(z);
+
+        myRigidBody.MovePosition(myRigidBody.position + velocity * Time.fixedDeltaTime);
+    }
+}
